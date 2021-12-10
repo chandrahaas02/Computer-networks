880b0ba9a2495c0bc3b7a121fae8528b5b3ff73f
Akhil Baja <74439261+akhilbaja@users.noreply.github.com>
2021-12-10 21:09:33 +0530

    Update multi-client.py

--- multi-client.py
+++ multi-client.py
@@ -1,40 +1,58 @@
 from _thread import *
 import threading
-import socket # import socket
+import socket
 import time
 import sys
 from datetime import datetime
+import os 
 
 
 def writing():
     while True:
-        msg = c_socket.recv(1024)
-        print(msg.decode())
-
+        msg=c_socket.recv(1024)
+        msg=msg.decode()
+        if msg[:4]=="file":
+            f = open("output.txt","w")
+            f.write(msg[4:])
+            f.close()
+            print("A file was send")
+        else:
+            print(msg)
 
 if __name__ == '__main__':
-    # Creating server socket
     c_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s_host = socket.gethostname()
-    ip = socket.gethostbyname(s_host)
-    s_PORT = 9992
+    s_host   = socket.gethostname()
+    ip       = socket.gethostbyname(s_host)
+    s_PORT   = 9992
 
-    print("Your IP :", ip)
-    s_host = input('Enter friend\'s IP address:')
+    print("Your IP :",ip)
+    s_host = input('Enter server\'s IP address:')
     name = input('Enter your name:')
-    c_socket.connect((s_host, s_PORT))
+    c_socket.connect((s_host,s_PORT))
 
     c_socket.sendall(name.encode())
     server_name = c_socket.recv(1024)
     server_name = server_name.decode()
-    print('Connected with ', server_name)
-    start_new_thread(writing, ())
+    print('Connected with ',server_name)
+    message = "get_list"
+    c_socket.sendall(message.encode())
+    start_new_thread(writing,())
     while True:
-        # print(server_name,":",message)
         message = input()
-        if message == 'quit':
+        if message == "file" :
+            recieve=input("Enter the names of recivers with space : ")
+            file = input("Enter the path to the file here : ")
+            f = open(file,'r')
+            message = f.read()
+            message= "file"+ " "+recieve+" "+":"+" "+ message
+        c_socket.sendall(message.encode())
+        if message =='quit':
             break
         c_socket.send(message.encode())
-        # data = c_socket.recv(1024)
-
+        data = c_socket.recv(1024) 
+        
     c_socket.close()
+
+    
+
+
